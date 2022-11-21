56d33b6b7ef1ce037cf06586e22ec22ebe305f31
Debora Cervieri Guterres <debora.guterres@gmail.com>
2022-11-20 21:41:21 -0300

    First commit.

---
+++ src/main/java/com/dio/flanelinha/FlanelinhaApplication.java
@@ -0,0 +1,13 @@
+package com.dio.flanelinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlanelinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlanelinhaApplication.class, args);
+	}
+
+}
